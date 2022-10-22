27542cc22f522a46e8d1e79e4779e27029afd481
BekzatBro <115966823+BekzatBro@users.noreply.github.com>
2022-10-22 13:46:18 +0600

    Revert "Media about 992px" (#19)
    Co-authored-by: TheAitbay <113280367+aitbek21v@users.noreply.github.com>

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const btn =document.querySelector(".footer-btn button")
-btn.addEventListener("click", () =>{
-    document.querySelector(".modal").style.display = "block"
-})
-const close =document.querySelector(".modal--bg__sq--color")
-close.addEventListener("click", () =>{
-    document.querySelector(".modal").style.display = "none"
-})
